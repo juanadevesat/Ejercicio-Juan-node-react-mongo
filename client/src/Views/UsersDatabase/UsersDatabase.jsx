3701c0d2ec882dcea443112181ecc3824a1a9f21
Juan Antonio Devesa Thomson <juanadevesat93@gmail.com>
2023-12-10 10:07:58 +0100

    backend link in database

--- client/src/Views/UsersDatabase/UsersDatabase.jsx
+++ client/src/Views/UsersDatabase/UsersDatabase.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import UserCard from "../../components/UserCard/UserCard";
 import { Link } from "react-router-dom";
 
-const backendUrl = 'https://react-nodejs-cloudbuilds-server-5lcjppv6ea-ew.a.run.app';
+const backendUrl = 'https://react-nodejs-cloudbuilds-server-j5isehf6lq-ew.a.run.app';
 
 function UsersDatabase() {
   const [users, setUsers] = useState([]);
